a3d6bd349946f42439b170a5f92933ce8fee058a
SandwhyDev <sandwhydev@gmail.com>
2022-03-17 15:44:02 +0700

    first update

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   <div className='w-screen min-h-screen bg-slate-100 flex flex-col items-center justify-center'>
+     hello
+   </div>
+  )
+}
