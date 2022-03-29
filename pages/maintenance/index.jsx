7ade2b9b8356e2140f0b584a5a129973250b514c
SandwhyDev <sandwhydev@gmail.com>
2022-03-29 19:36:07 +0700

    update

--- pages/maintenance/index.jsx
+++ pages/maintenance/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
+import Gif from "../../assets/flame-remote-working.gif";
 const Maintenance = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      Maintenance
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-white p-14">
+      <div>
+        <Image src={Gif} width={350} height={350} />
+      </div>
+      <h1 className="xl:text-2xl text-xl text-blue-500 text-center font-bold">
+        Mohon Maaf website <br /> sedang dalam perbaikan
+      </h1>
     </div>
   );
 };
