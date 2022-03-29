d5cf78875a960e972d38cec26c163ece757f17b7
SandwhyDev <sandwhydev@gmail.com>
2022-03-29 17:56:40 +0700

    update

--- pages/dokumentasi/[title].jsx
+++ pages/dokumentasi/[title].jsx
@@ -4,7 +4,6 @@ import Footer from "../../components/Footer";
 import CardImages from "../../components/CardImages";
 import CardImagesWide from "../../components/CardImageWide";
 import Head from "next/head";
-
 const DetailAcara = () => {
   return (
     <div className="w-screen min-h-screen flex flex-col ">
@@ -19,12 +18,12 @@ const DetailAcara = () => {
       <Navbar title="Dokumentasi" buttonBack back="/dokumentasiAcara" />
       <div className="w-full sm:ml-7 lg:ml-[50px] xl:ml-1 md:items-center md:justify-center  md:flex md:flex-wrap">
         <div className="w-full flex flex-wrap gap-[.9px]   sm:items-start sm:gap-2 sm:p-2   sm:max-w-[700px] lg:max-w-[900px] xl:max-w-[1300px]">
-          <CardImages image="https://images.pexels.com/photos/9802281/pexels-photo-9802281.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-          <CardImages image="https://images.pexels.com/photos/11129005/pexels-photo-11129005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-          <CardImages image="https://images.pexels.com/photos/10194726/pexels-photo-10194726.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-          <CardImages image="https://images.pexels.com/photos/10022592/pexels-photo-10022592.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-          <CardImages image="https://images.pexels.com/photos/11389730/pexels-photo-11389730.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-          <CardImages />
+          <CardImages image="/DSC09561.JPG" />
+          <CardImages image="/DSC09586.JPG" />
+          <CardImages image="/DSC09589.JPG" />
+          <CardImages image="/DSC09560.JPG" />
+          <CardImages image="/DSC09557.JPG" />
+          <CardImages image="/DSC09556.JPG" />
           <CardImages image="https://images.pexels.com/photos/4110472/pexels-photo-4110472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
           <CardImages image="https://images.pexels.com/photos/3867035/pexels-photo-3867035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
           <CardImages image="https://images.pexels.com/photos/5637599/pexels-photo-5637599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
